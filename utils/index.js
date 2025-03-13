afda60ca2cb74f867b1a39a0344e319a74dee955
Teeraphat Saengkum <kteeraphatt@gmail.com>
2025-03-13 20:35:44 +0700

    update frontend, update login button with google, facebook

--- utils/index.js
+++ utils/index.js
@@ -1,4 +1,5 @@
 export const backendUrl =
-  window.location.hostname === "localhost"
+  window.location.hostname === "localhost" ||
+  window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
     : "https://honey-backend.vercel.app";
