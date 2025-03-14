ec1813f45889a43ef85cae561f0d6f8a346cf325
patcha <pompatcharin@gmail.com>
2025-03-14 19:51:10 +0700

    change API services

--- utils/index.js
+++ utils/index.js
@@ -2,4 +2,4 @@ export const backendUrl =
   window.location.hostname === "localhost" ||
   window.location.hostname === "127.0.0.1"
     ? "http://localhost:5000"
-    : "https://honey-backend.vercel.app";
+    : "https://honey-backend.onrender.com";
